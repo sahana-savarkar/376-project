14d07d382610a5a5553a723dc0f89751c49b2c14
Nayda Farnsworth <125708157+njfarnsworth@users.noreply.github.com>
2024-12-06 12:07:57 -0500

    Made markov generator function and basic markov chain for directions.

--- project_376.py
+++ project_376.py
@@ -5,37 +5,29 @@ import random
 
 data = pd.read_csv('dessert_recipes.csv')
 
-recipe_list = data['recipe_name'].head(1000).tolist()
-recipe_list = [line.split() for line in recipe_list]
+recipe_name_list = data['recipe_name'].head(1000).tolist()
+recipe_name_list = [line.split() for line in recipe_name_list]
 
 ingredient_list = data['ingredients'].head(1000).tolist()
 ingredient_list = [line.split(",") for line in ingredient_list]
 
-markov_chain = defaultdict(lambda: defaultdict(int))
-markov_chain_recipe_name = defaultdict(lambda: defaultdict(int))
-
-for tokens in recipe_list:
-    for i in range(len(tokens) - 1):
-        current_word = tokens[i]
-        next_word = tokens[i + 1]
-        markov_chain_recipe_name[current_word][next_word] += 1
-
-for tokens in ingredient_list:
-    for i in range(len(tokens) - 1):
-        current_word = tokens[i]
-        next_word = tokens[i + 1]
-        markov_chain[current_word][next_word] += 1
-
-for current_word, transitions in markov_chain_recipe_name.items():
-    total_transitions = sum(transitions.values())
-    for next_word in transitions:
-        transitions[next_word] /= total_transitions
+directions_list = data['directions'].head(1000).tolist()
+directions_list = [line.split(" ") for line in directions_list]
 
 
-for current_word, transitions in markov_chain.items():
-    total_transitions = sum(transitions.values())
-    for next_word in transitions:
-        transitions[next_word] /= total_transitions
+
+def markov_chain_generator(data_list):
+    markov_chain = defaultdict(lambda: defaultdict(int))
+    for tokens in data_list:
+        for i in range(len(tokens) - 1):
+            current_word = tokens[i]
+            next_word = tokens[i + 1]
+            markov_chain[current_word][next_word] += 1
+    for current_word, transitions in markov_chain.items():
+        total_transitions = sum(transitions.values())
+        for next_word in transitions:
+            transitions[next_word] /= total_transitions
+    return markov_chain
 
 def generate_ingredient_list(chain, start_word, length):
     if start_word not in chain:
@@ -54,32 +46,41 @@ def generate_ingredient_list(chain, start_word, length):
     return ' '.join(result)
 
 
-
+markov_recipe_name = markov_chain_generator(recipe_name_list)
+markov_ingredient_list = markov_chain_generator(ingredient_list)
+markov_directions = markov_chain_generator(directions_list)
 
 recipe_start_word = "Apple"
 ingredient_start_word = "8 small Granny Smith apples"
 ingredient2_start_word =  "1 cup sliced fresh peaches"
+directions_start_word = "Peel"
 
 
 
-recipe_name = generate_ingredient_list(markov_chain_recipe_name, start_word=recipe_start_word, length=80)
+
+recipe_name_result = generate_ingredient_list(markov_recipe_name, start_word=recipe_start_word, length=80)
 
 ingredient_pattern = r'(\d+\s?\d*\/?\d*\s*(?:tablespoon|tbsp|teaspoon|tsp|cup|pounds|ounce|gram|g|liter|l)?\s*[a-zA-Z\-]+(?:\s?[a-zA-Z\-]+)*)'
-generated_list = generate_ingredient_list(markov_chain, start_word=ingredient_start_word, length=20)
-generated_list2 = generate_ingredient_list(markov_chain, start_word=ingredient2_start_word, length=20)
+ingredients_result = generate_ingredient_list(markov_ingredient_list, start_word=ingredient_start_word, length=20)
+ingredients_result2 = generate_ingredient_list(markov_ingredient_list, start_word=ingredient2_start_word, length=20)
 
-ingredients_list = re.findall(ingredient_pattern, generated_list)
-ingredients_list2 = re.findall(ingredient_pattern, generated_list2)
+ingredients_result = re.findall(ingredient_pattern, ingredients_result)
+ingredients_result2 = re.findall(ingredient_pattern, ingredients_result2)
+
+directions_result = generate_ingredient_list(markov_directions, start_word=directions_start_word, length=2000)
 
 
 
 print('\nRECIPE NAME: ')
-print(recipe_name)
+print(recipe_name_result)
 
 
 print("INGREDIENT LIST: \n")
-for ingredient in ingredients_list:
+for ingredient in ingredients_result:
     print(ingredient.strip())
-for ingredient in ingredients_list2:
+for ingredient in ingredients_result2:
     print(ingredient.strip())
 
+
+print("DIRECTIONS: \n")
+print(directions_result)
