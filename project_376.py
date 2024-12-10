b60fe4bb4556f2cac7158b91e6be01d949e9bba9
Nayda Farnsworth <125708157+njfarnsworth@users.noreply.github.com>
2024-12-09 21:43:17 -0500

    Removed bad ingredients and used random generator for recipe name start word

--- project_376.py
+++ project_376.py
@@ -10,7 +10,7 @@ recipe_name_list = data['recipe_name'].head(1000).tolist()
 recipe_name_list = [line.split() for line in recipe_name_list]
 
 ingredient_list = data['ingredients'].head(1000).tolist()
-ingredient_list = [line.split(",") for line in ingredient_list]
+ingredient_list = [line.split() for line in ingredient_list]
 
 directions_list = data['directions'].head(1000).tolist()
 directions_list = [line.split(" ") for line in directions_list]
@@ -51,9 +51,9 @@ markov_recipe_name = markov_chain_generator(recipe_name_list)
 markov_ingredient_list = markov_chain_generator(ingredient_list)
 markov_directions = markov_chain_generator(directions_list)
 
-recipe_start_word = "Apple"
-ingredient_start_word = "½ cup unsalted butter"
-ingredient2_start_word =  "6 cups thinly sliced apples"
+recipe_start_word = random.choice(list(markov_recipe_name.keys()))
+ingredient_start_word = "butter"
+ingredient2_start_word =  "apples"
 directions1_start_word = "Peel"
 directions2_start_word = "Mix"
 directions3_start_word = "Beat"
@@ -63,8 +63,8 @@ directions3_start_word = "Beat"
 recipe_name_result = generate_ingredient_list(markov_recipe_name, start_word=recipe_start_word, length=80)
 
 ingredient_pattern = r'(\d+\s?\d*\/?\d*\s*(?:tablespoon|tbsp|teaspoon|tsp|cup|pounds|ounce|gram|g|liter|l)?\s*[a-zA-Z\-]+(?:\s?[a-zA-Z\-]+)*)'
-ingredients_result = generate_ingredient_list(markov_ingredient_list, start_word=ingredient_start_word, length=20)
-ingredients_result2 = generate_ingredient_list(markov_ingredient_list, start_word=ingredient2_start_word, length=20)
+ingredients_result = generate_ingredient_list(markov_ingredient_list, start_word=ingredient_start_word, length=100)
+ingredients_result2 = generate_ingredient_list(markov_ingredient_list, start_word=ingredient2_start_word, length=100)
 
 ingredients_result = re.findall(ingredient_pattern, ingredients_result)
 ingredients_result2 = re.findall(ingredient_pattern, ingredients_result2)
@@ -72,6 +72,22 @@ ingredients_result2 = re.findall(ingredient_pattern, ingredients_result2)
 directions_result1 = generate_ingredient_list(markov_directions, start_word=directions1_start_word, length=100)
 directions_result2 = generate_ingredient_list(markov_directions, start_word=directions2_start_word, length=100)
 directions_result3 = generate_ingredient_list(markov_directions, start_word=directions3_start_word, length=100)
+
+invalid_ingredient_list = ['1', '1/4', '10', '12', '18', '2', '20', '3', '4', '5', '6', '7', '8', '9', 'peeled', 'sliced', 'cored', 'softened', 'halved', 'crushed', 'shredded', 'chopped', 'grated', 'lightly', 'firm', 'thinly', 'beaten', 'drained', 'packed', 'desired', 'prepared', 'freshly', 'quartered', 'needed', 'ripe', 'frozen', 'baked', 'split', 'melted', 'salted', 'unbaked', 'warmed', 'mixed', 'ground', 'inch', 'pieces', 'tablespoon', 'cup', 'ounce', 'cold', 'a']
+
+def ingredient_end(ingredient):
+    last_word = ingredient.strip().split()[-1]
+    return last_word 
+
+def truncate_invalid_ingredients(ingredients):
+    valid_ingredients = []
+    for ingredient in ingredients:
+        if ingredient_end(ingredient) not in invalid_ingredient_list:
+            valid_ingredients.append(ingredient)
+    return valid_ingredients
+
+ingredients_result = truncate_invalid_ingredients(ingredients_result)
+ingredients_result2 =  truncate_invalid_ingredients(ingredients_result2)
 
 
 def add_bullet_points(paragraph, bullet='•'):
